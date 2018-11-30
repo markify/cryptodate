f852d81b42a4feec5eb6b05047528d4a89048e19
Mark Soriano <merksmanship@gmail.com>
2018-11-30 15:35:34 -0800

    handle states and input

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 import DatePicker from 'react-datepicker'
 import './img/bitcoin.png'
 
-const Index = () => {
-	return (
+class Index extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  startDate: new Date(),
+		  price: 1
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.onInputChange = this.onInputChange.bind(this)
+	  }
+	
+	  handleChange(date) {
+		this.setState( { startDate: date}, () => console.log(this.state.startDate) );
+	  }
+
+	  onInputChange(event){
+		this.setState({ price: event.target.value } , () => console.log(this.state.price) );
+	  }
+// event.target.value for input
+	render() {
+		return (
 		<React.Fragment>
 			<section id="index">
 				<div className="container">
@@ -16,10 +35,17 @@ const Index = () => {
 						<h2> Enter transactions </h2>
 
 						<label> Price </label>
-						<input type="text" name="amount"></input>
+						<input type="text" name="amount" value={this.props.price}  onChange={this.onInputChange} ></input>
 
 						<label> Date </label>
-						<DatePicker />
+						<DatePicker
+						selected={this.state.startDate}
+						onChange={this.handleChange}
+						peekNextMonth
+						showMonthDropdown
+						showYearDropdown
+						dropdownMode="select"
+						/>
 						<Link to={`result`} >
 						<button type="submit" className="check-btn">
               				Check Profits
@@ -30,7 +56,8 @@ const Index = () => {
 			</section>
 			<Footer/>
 		</React.Fragment>
-	);
-};
+		);
+	} 	
+}
 
 export default Index;
